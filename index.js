f332b309583d26115c88ecd39bd5442bbf2a46c8
MrPerry365 <1987nowhere@gmail.com>
2022-06-16 17:00:45 -0400

    add initial inquirer question prompt

--- index.js
+++ index.js
@@ -8,13 +8,46 @@ const Engineer = require("../lib/Engineer.js");
 const Intern = require("../lib/Intern.js");
 const Manager = require("../lib/Manager.js");
 
-inquirer
+function initialPrompt() {
+return inquirer
 .prompt ([
     {
-        type: 'input'
-        name: 'Employee-name'
-        message:
-
-    }
+        type: 'input',
+        name: 'name_prompt',
+        message:'What is the name of the employee?',
+        validate: nameAdded => {
+            if (nameAdded) {
+                return true;}
+                else {console.log ('Please add a name to the employee prompt!');
+            return false;
+        }    
+        }
+    },
     
+    {
+        type: 'input',
+        name: 'id_prompt',
+        message:'What is the id of the employee?',
+        validate: idAdded => {
+            if (idAdded) {
+                return true;}
+                else {console.log ('Please add an id to the employee prompt!');
+            return false;
+        }    
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'email_prompt',
+        message:'What is the email of the employee?',
+        validate: emailAdded => {
+            if (emailAdded) {
+                return true;}
+                else {console.log ('Please add an email to the employee prompt!');
+            return false;
+        }    
+        }
+    },
 ])
+}
