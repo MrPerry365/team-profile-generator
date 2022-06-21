7b2be4f6c190d48821a5b58b352001e853e4d4d6
MrPerry365 <1987nowhere@gmail.com>
2022-06-20 22:06:17 -0400

    add node to script, add working functionality to prompts, data is now gathered for each member

--- index.js
+++ index.js
@@ -4,145 +4,324 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
 
-// const Employee = require("../lib/Employee.js"); 
-const Engineer = require("../lib/Engineer.js");
-const Intern = require("../lib/Intern.js");
-const Manager = require("../lib/Manager.js");
+const Employee = require("./lib/Employee.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+const Manager = require("./lib/Manager.js");
 const { create } = require("domain");
+const { info } = require("console");
+const team = [];
 
 
 
-function managerPrompt() {
-  return inquirer.prompt([
-    // prompt for the team managers name //
-    {
-      type: "input",
-      name: "name_prompt",
-      message: "What is the name of the team Manager?",
-      validate: (nameAdded) => {
-        if (nameAdded) {
-          return true;
-        } else {
-          console.log("Please add a name to the team manager prompt!");
-          return false;
-        }
-      },
-    },
-    // prompt for a team manager id //
-    {
-      type: "input",
-      name: "id_prompt",
-      message: "What is the id of the team manager?",
-      validate: (idAdded) => {
-        if (idAdded) {
-          return true;
-        } else {
-          console.log("Please add an id to the manager prompt!");
-          return false;
-        }
-      },
-    },
-    // prompt for the manager email //
-    {
-      type: "input",
-      name: "email_prompt",
-      message: "What is the email of the team manager?",
-      validate: (emailAdded) => {
-        if (emailAdded) {
-          return true;
-        } else {
-          console.log("Please add an email to the manager prompt!");
-          return false;
-        }
-      },
-    },
-    // prompt for manager office number //
-    {
-      type: "input",
-      name: "officeNumber_prompt",
-      message: "What is the office number of the team manager?",
-      validate: (officeNumberAdded) => {
-        if (officeNumberAdded) {
-          return true;
-        } else {
-          console.log("Please add an office number to the manager prompt!");
-          return false;
-        }
-      },
-    },
-
-    // use the answers to create a team manager //
-
-  ])
-  .then (answers => {
-    console.log (answers);
-    const teamManager = new Manager(answers.nameAdded, answers.idAdded, answers.emailAdded, answers.officeNumberAdded);
-    team.push(teamManager);
-    createTeamMember();
-  })
-}
-//----------------------------------------------------------------
-
-// create a create a team function that prompts for the type of team member and then to get their information // 
-
-createTeamMember () {
-  inquirer.prompt([
-
-    {
-      type: 'list',
-      name: ' teamMemberType',
-      message: 'What type of team member would you like to create?',
-      choices: ['intern', 'engineer',]
-    }
-    
-
-  ])
-  .then(teamMember => {
-    switch (teamMember.teamMemberType) {
-     
-      case 'intern': internPrompt();
-      break;
-      case 'engineer': engineerPrompt();
-      break;
-    }
-  })
+const main = async () => {
+  //start here
+  employeePrompt("Manager");
 
 }
 
-internPrompt() {
-
-inquirer.prompt([
-
-{
-  type: 'input',
-  name: 'internName',
-  message: ' What is the name of the intern?',
-  validate: (nameAdded) => {
-    if (nameAdded) {
-      return true;
-    } else {
-      console.log("Please add a name to the team intern prompt!");
-      return false;
-    }
+async function enterAnother() {
+  var doCreate = false;
+ return inquirer.prompt([
+  //prompt for entering another team member.  prompt for info if so, display cards if name_prompt
+  {
+    type: "list",
+    name: "createAnotherVal",
+    choices: ["Engineer", "Intern", "No"], 
+    message: "Add another team member? (Engineer, Intern, No)",
+    validate: (createAnotherVal_added) => {
+      if(createAnotherVal_added) {
+        return true;
+      } else {
+        console.log("Please choose whether to create another team member or not");
+        return false;
+      }
+    },
   },
-  
-},
-
-{
-  type: 'input',
-  name: 'internSchool',
-  message: 'What is the name of the school for this intern?',
-  validate: (schoolAdded) => {
-    if (schoolAdded) {
-      return true;
-    } else {
-      console.log("Please add a school name to the team intern prompt!");
-      return false;
-    }
-  },
+ ]).then(answers => {
+    if(answers.createAnotherVal === "Engineer") {
+      employeePrompt("Engineer");
+      doCreate = true;
+    } else if(answers.createAnotherVal === "Intern") {
+      employeePrompt("Intern");
+      doCreate = true;
+    } else {  // 'No' option - do not create more employees, create HTML file displaying team info
+      console.log(team);
+      //display to cards
+      var writeString = "";
+      for(let i = 0; i < team.length; i++) {
+        // $("#card-list").append(teams[i].writeEmployeeCard());
+        writeString += team[i].writeEmployeeCard();
+      }
+      writeString = "<html><body>" + writeString + "</body></html>"; 
+      console.log(writeString);
+      fs.writeFile("test.html", writeString, function(err) {
+        if(err) {
+          return console.log(err);
+        }
+        console.log("test.output successfully written to project directory");
+      } )
+    } 
+ });
 }
-])
+
+async function employeePrompt(role) {
+  // console.log(team);
+  return inquirer
+    .prompt([
+      // prompt for the team employees name //
+      {
+        type: "input",
+        name: "name_prompt",
+        message: "What is the name of the team employee?",
+        validate: (nameAdded) => {
+          if (nameAdded) {
+            return true;
+          } else {
+            console.log("Please add a name to the team employee prompt!");
+            return false;
+          }
+        },
+      },
+      // prompt for a team employee id //
+      {
+        type: "input",
+        name: "id_prompt",
+        message: "What is the id of the team employee?",
+        validate: (idAdded) => {
+          if (idAdded) {
+            return true;
+          } else {
+            console.log("Please add an id to the employee prompt!");
+            return false;
+          }
+        },
+      },
+      // prompt for the employee email //
+      {
+        type: "input",
+        name: "email_prompt",
+        message: "What is the email of the team employee?",
+        validate: (emailAdded) => {
+          if (emailAdded) {
+            return true;
+          } else {
+            console.log("Please add an email to the employee prompt!");
+            return false;
+          }
+        },
+      },
+    ])
+    .then((answers) => {
+      //console.log("Employee: " + answers.name_prompt + ", id: " + answers.id_prompt + ", email = " + answers.email_prompt);
+      if (role === "Manager") {
+      managerPrompt(
+          answers.name_prompt,
+          answers.id_prompt,
+          answers.email_prompt
+        );
+      } else if (role === "Intern") {
+       internPrompt(
+          answers.name_prompt,
+          answers.id_prompt,
+          answers.email_prompt
+        );
+      } else if (role === "Engineer") {
+       engineerPrompt(
+          answers.name_prompt,
+          answers.id_prompt,
+          answers.email_prompt
+        );
+      }
+    });
 }
 
+ function managerPrompt(name, id, email) {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "officeNumber_prompt",
+        message: "What is the office number of the team manager?",
+        validate: (officeNumberAdded) => {
+          if (officeNumberAdded) {
+            return true;
+          } else {
+            console.log("Please add an office number to the manager prompt!");
+            return false;
+          }
+        },
+      },
 
-managerPrompt();
+      // use the answers to create a team manager //
+    ])
+    .then((answers) => {
+      //console.log(answers);
+      const teamManager = new Manager(
+        name,
+        id,
+        email,
+        answers.officeNumber_prompt
+      );
+      team.push(teamManager);
+      enterAnother();
+    });
+}
+
+async function internPrompt(name, id, email) {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "school_prompt",
+        message: "What school does the intern attend?",
+        validate: (school_added) => {
+          if (school_added) {
+            return true;
+          } else {
+            console.log("Please enter the intern's school!");
+            return false;
+          }
+        },
+      },
+
+      // use the answers to create a team manager //
+    ])
+    .then((answers) => {
+      console.log(answers);
+      team.push(new Intern(name, id, email, answers.school_prompt));
+      enterAnother();
+    });
+}
+
+async function engineerPrompt(name, id, email) {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "github_prompt",
+        message: "What is the engineer's Github account?",
+        validate: (github_added) => {
+          if (github_added) {
+            return true;
+          } else {
+            console.log("Please add an Github account!");
+            return false;
+          }
+        },
+      },
+
+      // use the answers to create a team manager //
+    ])
+    .then((answers) => {
+      console.log(answers);
+      team.push(new Engineer(name, id, email, answers.github_prompt));
+      enterAnother();
+    });
+}
+
+main();
+
+
+
+
+
+// .then (answers => {
+//     console.log (answers);
+//     const teamManager = new Manager(answers.nameAdded, answers.idAdded, answers.emailAdded, answers.officeNumberAdded);
+//     team.push(teamManager);
+//     createTeamMember();
+//   })
+
+// prompt for manager office number //
+
+//     function managerPrompt() {
+//       return inquirer.prompt([
+
+//     {
+//       type: "input",
+//       name: "officeNumber_prompt",
+//       message: "What is the office number of the team manager?",
+//       validate: (officeNumberAdded) => {
+//         if (officeNumberAdded) {
+//           return true;
+//         } else {
+//           console.log("Please add an office number to the manager prompt!");
+//           return false;
+//         }
+//       },
+//     },
+
+//     // use the answers to create a team manager //
+
+//   ])
+//   .then (answers => {
+//     console.log (answers);
+//     const teamManager = new Manager(answers.nameAdded, answers.idAdded, answers.emailAdded, answers.officeNumberAdded);
+//     team.push(teamManager);
+//     createTeamMember();
+//   })
+// }
+// //----------------------------------------------------------------
+
+// // create a create a team function that prompts for the type of team member and then to get their information //
+
+// createTeamMember () {
+//   inquirer.prompt([
+
+//     {
+//       type: 'list',
+//       name: ' teamMemberType',
+//       message: 'What type of team member would you like to create?',
+//       choices: ['intern', 'engineer',]
+//     }
+
+//   ])
+//   .then(teamMember => {
+//     switch (teamMember.teamMemberType) {
+
+//       case 'intern': internPrompt();
+//       break;
+//       case 'engineer': engineerPrompt();
+//       break;
+//     }
+//   })
+
+// }
+
+// internPrompt() {
+
+// inquirer.prompt([
+
+// {
+//   type: 'input',
+//   name: 'internName',
+//   message: ' What is the name of the intern?',
+//   validate: (nameAdded) => {
+//     if (nameAdded) {
+//       return true;
+//     } else {
+//       console.log("Please add a name to the team intern prompt!");
+//       return false;
+//     }
+//   },
+
+// },
+
+// {
+//   type: 'input',
+//   name: 'internSchool',
+//   message: 'What is the name of the school for this intern?',
+//   validate: (schoolAdded) => {
+//     if (schoolAdded) {
+//       return true;
+//     } else {
+//       console.log("Please add a school name to the team intern prompt!");
+//       return false;
+//     }
+//   },
+// }
+// ])
+// }
+
+// managerPrompt();
